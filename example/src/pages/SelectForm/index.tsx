eec16def91e6e60e63424fea361a9c315a8fef88
Gustavo Vilas Boas <gustavo.boas@startaideia.com.br>
2020-09-17 21:24:29 -0300

    doc: add select shape example

--- example/src/pages/SelectForm/index.tsx
+++ example/src/pages/SelectForm/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, Select, Submit } from 'react-forms'
+import { Form, Select, Shape, Submit } from 'react-forms'
 
 const data = [
   {
@@ -14,21 +14,41 @@ const data = [
 
 function SelectForm() {
   return (
-    <Form onSubmit={(values) => console.log(values)}>
-      <Select
-        name='gender'
-        label='Sexo'
-        isRequired
-        help='Você não precisa se identificar com a opção selecionada'
-      >
-        {data.map(({ label, value }) => (
-          <Select.Option key={value} label={label} value={value} />
-        ))}
-      </Select>
-      <Form.Footer>
-        <Submit>Enviar</Submit>
-      </Form.Footer>
-    </Form>
+    <React.Fragment>
+      <Form onSubmit={(values) => console.log(values)}>
+        <Select
+          name='gender'
+          label='Sexo'
+          isRequired
+          help='Você não precisa se identificar com a opção selecionada'
+        >
+          {data.map(({ label, value }) => (
+            <Select.Option key={value} label={label} value={value} />
+          ))}
+        </Select>
+        <Form.Footer>
+          <Submit>Enviar</Submit>
+        </Form.Footer>
+      </Form>
+      <h2>Select com Shape</h2>
+      <Form onSubmit={(values) => console.log(values)}>
+        <Shape path='user'>
+          <Select
+            name='gender'
+            label='Sexo'
+            isRequired
+            help='Você não precisa se identificar com a opção selecionada'
+          >
+            {data.map(({ label, value }) => (
+              <Select.Option key={value} label={label} value={value} />
+            ))}
+          </Select>
+        </Shape>
+        <Form.Footer>
+          <Submit>Enviar</Submit>
+        </Form.Footer>
+      </Form>
+    </React.Fragment>
   )
 }
 
