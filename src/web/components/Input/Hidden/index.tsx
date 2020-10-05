a7a652fb35a149927940f97637d28be1f8383495
Gustavo Vilas Boas <gustavo.boas@startaideia.com.br>
2020-10-05 16:33:35 -0300

    fix: remove margins from input hidden

--- src/web/components/Input/Hidden/index.tsx
+++ src/web/components/Input/Hidden/index.tsx
@@ -1,8 +1,78 @@
-import React from 'react'
-import Text from '../Text'
+import React, { useCallback, useMemo, useState } from 'react'
 
-function Hidden({ ...props }) {
-  return <Text type='hidden' {...props} />
+import { applyMask, useValidation, useControl } from 'packages'
+import { parseClassName } from 'web/utils'
+
+function Hidden({
+  name,
+  mask = '',
+  initialValue = '',
+  onChange = undefined,
+  onFocus = undefined,
+  onBlur = undefined,
+  props,
+  ...rest
+}: any) {
+  const { setValue, value } = useControl(name, { initialValue })
+  const { valid, invalid } = useValidation(name, rest)
+  const [touched, setTouched] = useState<boolean>(false)
+  const [focus, setFocus] = useState<boolean>(false)
+
+  /* Input class name */
+  const className = useMemo(
+    () => parseClassName({ valid, focus, touched, invalid }),
+    [focus, touched, invalid, valid]
+  )
+
+  /**
+   * Handle input change
+   *
+   */
+  const handleChange = useCallback(
+    function (e) {
+      const inputValue = mask ? applyMask(mask, e.target.value) : e.target.value
+      setValue(inputValue)
+      if (onChange) onChange(inputValue)
+    },
+    [setValue]
+  )
+
+  /**
+   * Handle input focus
+   *
+   */
+  const handleFocus = useCallback(
+    function () {
+      setFocus(true)
+      if (onFocus) onFocus()
+    },
+    [setFocus]
+  )
+
+  /**
+   * Handle input blur
+   *
+   */
+  const handleBlur = useCallback(
+    function () {
+      setTouched(true)
+      setFocus(false)
+      if (onBlur) onBlur()
+    },
+    [setFocus, setTouched]
+  )
+
+  return (
+    <input
+      className={`${className}`}
+      onChange={handleChange}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      value={value}
+      type='hidden'
+      {...props}
+    />
+  )
 }
 
 export default Hidden
