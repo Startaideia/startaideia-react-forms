3977ad5693fffe5fa487c55ed9fc8494807bec33
Gustavo Vilas Boas <gustavo.boas@startaideia.com.br>
2020-10-26 17:17:57 -0300

    fix: money mask

--- example/src/pages/MoneyForm/index.tsx
+++ example/src/pages/MoneyForm/index.tsx
@@ -4,8 +4,9 @@ import { Submit, Form, Input } from '@startaideia/react-forms'
 
 function MoneyForm() {
   return (
-    <Form initialValue={{ money: 12 }}>
+    <Form initialValue={{ money: 12, money_2: 'a' }}>
       <Input.Money name='money' label='Dinheiro' isRequired />
+      <Input.Money name='money_2' label='Dinheiro' isRequired />
       <Form.Footer>
         <Submit>Enviar</Submit>
       </Form.Footer>
--- src/web/components/Input/Money/index.tsx
+++ src/web/components/Input/Money/index.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import Text from '../Text'
 
 function format(str) {
+  const number = parseInt(str.toString().replace(/[\D]+/g, ''))
+
+  if (isNaN(number)) {
+    return ''
+  }
+
   return (
     'R$ ' +
-    (parseInt(str.toString().replace(/[\D]+/g, '')) / 100)
+    (number / 100)
       .toFixed(2)
       .replace('.', ',')
       .replace(/(\d)(?=(\d{3})+,)/g, '$1.')
